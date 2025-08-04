ff361f10ff77e9b8ffb7e2d2f1191541fd1183a4
Guillaume "Elektordi" Genty <github@elektordi.net>
2025-08-04 18:19:56 +0200

    Fix ip address used in tests

--- fastping_test.go
+++ fastping_test.go
@@ -165,7 +165,7 @@ func TestRun(t *testing.T) {
 			t.Fatalf("AddIP failed: %v", err)
 		}
 
-		if err := p.AddIP("127.0.0.100"); err != nil {
+		if err := p.AddIP("198.51.100.100"); err != nil {
 			t.Fatalf("AddIP failed: %v", err)
 		}
 
@@ -179,7 +179,7 @@ func TestRun(t *testing.T) {
 			called = true
 			if ip.String() == "127.0.0.1" {
 				found1 = true
-			} else if ip.String() == "127.0.0.100" {
+			} else if ip.String() == "198.51.100.100" {
 				found100 = true
 			} else if ip.String() == "::1" {
 				foundv6 = true
@@ -204,7 +204,7 @@ func TestRun(t *testing.T) {
 			t.Fatalf("Pinger `127.0.0.1` didn't respond")
 		}
 		if found100 {
-			t.Fatalf("Pinger `127.0.0.100` responded")
+			t.Fatalf("Pinger `198.51.100.100` responded")
 		}
 		if !foundv6 {
 			t.Fatalf("Pinger `::1` didn't responded")
